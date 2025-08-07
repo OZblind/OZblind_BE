077897119ef314865c95a6720ddfd420369b89f1
최재현 <zip020314@gmail.com>
2025-08-07 14:37:01 +0900

    boards 푸쉬 받기 전

--- backend/apps/bookmarks/models.py
+++ backend/apps/bookmarks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Bookmark(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='bookmarks')
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE, related_name='bookmarks')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User: {self.user.username} bookmarked Post: '{self.post.title}'"
+
+    class Meta:
+        constraints = [
+            # 중복 북마크를 방지하는 가장 중요한 규칙입니다.
+            models.UniqueConstraint(fields=['user', 'post'], name='unique_user_post_bookmark')
+        ]
+        ordering = ['-created_at']
--- backend/apps/bookmarks/views.py
+++ backend/apps/bookmarks/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from backend.apps.posts.models import Post
+from .models import Bookmark
+from .serializers import BookmarkSerializer
+
+class BookmarkAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        
+        serializer = BookmarkSerializer(data={'post': post.id})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        
+        # 명세서에 맞게 응답 데이터를 구성합니다.
+        response_data = {
+            'id': serializer.instance.id,
+            'post_id': serializer.instance.post.id
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        
+        bookmark = get_object_or_404(Bookmark, user=request.user, post=post)
+        bookmark.delete()
+        return Response({"message": "삭제완료"}, status=status.HTTP_204_NO_CONTENT)
