7a79e7a19f404bd86a79d04ccefce4ca914ac1a0
daeun0608 <glace0608@naver.com>
2025-08-07 17:38:57 +0900

    Feat : post, ntf model 구현

--- backend/apps/posts/models.py
+++ backend/apps/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    board = models.ForeignKey('boards.Board', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=5000)
+    image = models.CharField(max_length=255, null=True)
+    views = models.IntegerField(default=0)
+    likes_cnt = models.IntegerField(default=0)
+    dislikes_cnt = models.IntegerField(default=0)
+    bookmark_cnt = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
