5e0c31546ffaabaac80be9f62bc00afd3401e6bd
daeun0608 <glace0608@naver.com>
2025-08-08 18:39:28 +0900

    Feat : notifications

--- backend/apps/notifications/urls.py
+++ backend/apps/notifications/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import NotificationList, NotificationCheck, NotificationCheckDetail
 
 urlpatterns = [
-    path('', NotificationList.as_view()),
-    path('check', NotificationCheck.as_view()),
-    path('check/<int:pk>', NotificationCheckDetail.as_view()),
+    path('', NotificationList.as_view(), name='ntf-list'),
+    path('check', NotificationCheck.as_view(), name='ntf-check'),
+    path('check/<int:pk>', NotificationCheckDetail.as_view(), name='ntf-check-detail'),
 ]
