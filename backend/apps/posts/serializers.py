9fb7f9671a2e51be6ea78369fa5bb27a28d248dd
Syapa723 <birdragon2@gmail.com>
2025-08-12 11:17:46 +0900

    게시글 정렬, 게시글 조회(댓글포함) 기능 구현
    # Conflicts:
    #	backend/apps/posts/serializers.py

--- backend/apps/posts/serializers.py
+++ backend/apps/posts/serializers.py
@@ -1,22 +1,64 @@
 from rest_framework import serializers
 from .models import Post
-from ..notifications.models import Notification
+from comments.model import Comment
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'post',
+            'user',
+            'parent',
+            'content',
+            'like_count',
+            'dislike_count',
+            'created_at',
+            'updated_at'
+        ]
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'post',
+            'user',
+            'parent',
+            'content',
+            'like_count',
+            'dislike_count',
+            'created_at',
+            'updated_at',
+            'children'
+        ]
+
 
 
 class PostSerializer(serializers.ModelSerializer):
+
+    root_comment = CommentSerializer()
+
     class Meta:
         model = Post
-        fields = '__all__'
-        read_only_fields = (
+        fields = [
             'id',
+            'board',
             'user',
+            'title',
+            'content',
+            'image',
             'view_count',
             'like_count',
             'dislike_count',
             'bookmark_count',
             'created_at',
-            'updated_at'
-        )
+            'updated_at',
+            'root_comment'
+        ]
+
+        def get_root_comment(self, post_obj):
+            toplevel_comments = post_obj.comment_set.filter(parent_isnull = True)
+            return CommentSerializer(toplevel_comments, many=True).data
 
 # 알림기능에 포스트 정보 제한
 class NotificationPostSerializer(serializers.ModelSerializer):
