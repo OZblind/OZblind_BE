d2d3a3274821de90a4f954d9276a8f76a49f91ff
Syapa723 <birdragon2@gmail.com>
2025-08-12 11:17:46 +0900

    게시글 정렬, 게시글 조회(댓글포함) 기능 구현

--- backend/apps/posts/serializers.py
+++ backend/apps/posts/serializers.py
@@ -1,17 +1,60 @@
 from rest_framework import serializers
 from .models import Post
+from comments.model import Comment
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'post',
+            'user',
+            'parent',
+            'content',
+            'like_count',
+            'dislike_count',
+            'created_at',
+            'updated_at'
+        ]
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = [
+            'id',
+            'post',
+            'user',
+            'parent',
+            'content',
+            'like_count',
+            'dislike_count',
+            'created_at',
+            'updated_at',
+            'children'
+        ]
 
 class PostSerializer(serializers.ModelSerializer):
+
+    root_comment = CommentSerializer()
+
     class Meta:
         model = Post
-        fields = '__all__'
-        read_only_fields = (
+        fields = [
             'id',
+            'board',
             'user',
+            'title',
+            'content',
+            'image',
             'view_count',
             'like_count',
             'dislike_count',
             'bookmark_count',
             'created_at',
-            'updated_at'
-        )
+            'updated_at',
+            'root_comment'
+        ]
+
+        def get_root_comment(self, post_obj):
+            toplevel_comments = post_obj.comment_set.filter(parent_isnull = True)
+            return CommentSerializer(toplevel_comments, many=True).data
+
--- backend/apps/posts/views.py
+++ backend/apps/posts/views.py
@@ -1,3 +1,4 @@
+#from django.db.models import F 추후 인기게시글 좋아요 반영시 작성예정
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Post
@@ -8,6 +9,21 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def _get_popular_queryset(self, queryset):
+        return queryset.order_by('-view_count', '-created_at')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        ordering = self.request.query_params.get('ordering', None)
+
+        if ordering == 'popular':
+            return self._get_popular_queryset(queryset)
+
+        if ordering == 'created_at':
+            return  queryset.order_by('-created_at')
+
+        return queryset.order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
