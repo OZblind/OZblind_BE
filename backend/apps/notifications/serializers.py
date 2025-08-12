f4c97011c4e2448da0f1f5944fc0359fa163be62
daeun0608 <glace0608@naver.com>
2025-08-12 13:41:08 +0900

    Feat : 2차 댓글 기능 추가

--- backend/apps/notifications/serializers.py
+++ backend/apps/notifications/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Notification
 
-class NftListSerializer(ModelSerializer):
+class NtfListSerializer(ModelSerializer):
     class Meta:
         model = Notification
         exclude = ('user',)
