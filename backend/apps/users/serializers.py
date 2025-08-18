c7732b1c9dcaaf130ae8f21f80ab2016bb221b35
daeun0608 <glace0608@naver.com>
2025-08-18 14:40:44 +0900

    Feat : 1차 태그, 시리얼라이저 작업
    # Conflicts:
    #	uv.lock
    
    # Conflicts:
    #	backend/apps/posts/serializers.py
    #	backend/apps/posts/tests.py
    
    # Conflicts:
    #	backend/apps/users/serializers.py

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -10,6 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'profile_image', 'role', 'is_active']
+
 class ActivateSerializer(serializers.Serializer):
     id_token_str = serializers.CharField(write_only=True)
     cohort_number = serializers.CharField(write_only=True)
@@ -21,3 +22,13 @@ class UserWithTokenSerializer(serializers.Serializer):
 
     class Meta:
         fields = UserSerializer.Meta.fields + ['access', 'refresh', 'is_active', 'id','email']
+
+
+
+# 태그 정보가 필요한 경우 정보 필터링에 사용할 시리얼라이저
+class UserTagSerializer(serializers.ModelSerializer):
+    tag_class=serializers.CharField(source='oz_keys.get.tag_class', read_only=True)
+    tag_number=serializers.IntegerField(source='oz_keys.get.tag_number', read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', 'tag_class', 'tag_number']
