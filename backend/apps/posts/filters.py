6d24d0e36cb78c38ae01a4fec622c067db56cdeb
Syapa723 <birdragon2@gmail.com>
2025-08-25 01:34:38 +0900

    태그필터링 기능 오류 수정

--- backend/apps/posts/filters.py
+++ backend/apps/posts/filters.py
@@ -1,13 +1,20 @@
 import django_filters
-from ..users.models import User
 from .models import Post
 
 class PostFilter(django_filters.FilterSet):
     # Post 모델에 대한 커스텀 필터셋
     # 1. 필드네임 user__tag_number : 필터가 Post 모델의 'user'를 따라가서 해당 유저의 'tag_number' 필드를 필터링 하도록 지정
-    user_tag_number = django_filters.NumberFilter(field_name='user__tag_number')
-    user_tag_name = django_filters.CharFilter(field_name='user__tag_class')
+    user_tag_number = django_filters.NumberFilter(field_name='user__oz_keys__tag_number')
+    user_tag_class = django_filters.CharFilter(field_name='user__oz_keys__tag_class')
 
     class Meta:
         model = Post
         fields = ['board']
+
+    # 2.'user_tag_number'파라미터를 처리할 메소드 직접 정의
+    def filter_by_tag_number(self, queryset, name, value):
+        return queryset.filter(user__oz_keys__tag_number=value)
+
+    # 3. 'user_tag_class'파라미터를 처리할 메소드 직접 정의
+    def filter_by_tag_class(self, queryset, name, value):
+        return queryset.filter(user__oz_keys__tag_class=value)
